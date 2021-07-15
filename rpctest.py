68db0db219cf8ad92fe6d2e305722549ff98a174
alex-wii <ggg.combo@gmail.com>
2021-07-15 16:01:10 +0800

    Initial import

---
+++ rpctest.py
@@ -0,0 +1,9 @@
+﻿from jsonrpcclient import request
+from time import sleep
+import random
+while True:
+    num=random.randint(0,1000)
+    a = {"ordernum":f"a{num:04}","resp":"010002000300040000500"}
+    
+    response = request("http://localhost:5000","jsonrpc_addorder",order=a)
+    sleep(2)
